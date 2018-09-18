3883e85796241b8168ff0cc692895515b4db82c6
Reimu747 <1099169475@qq.com>
2018-09-18 23:43:01 +0800

    2018.9.18 By Reimu747 At home

--- src/main/java/cn/xuyue/shirowithspringboot/vo/User.java
+++ src/main/java/cn/xuyue/shirowithspringboot/vo/User.java
@@ -1,22 +1,38 @@
 package cn.xuyue.shirowithspringboot.vo;
 
-public class User {
+public class User
+{
     private String username;
     private String password;
+    private String id;
 
-    public String getUsername() {
+    public String getId()
+    {
+        return id;
+    }
+
+    public void setId(String id)
+    {
+        this.id = id;
+    }
+
+    public String getUsername()
+    {
         return username;
     }
 
-    public void setUsername(String username) {
+    public void setUsername(String username)
+    {
         this.username = username;
     }
 
-    public String getPassword() {
+    public String getPassword()
+    {
         return password;
     }
 
-    public void setPassword(String password) {
+    public void setPassword(String password)
+    {
         this.password = password;
     }
 }
